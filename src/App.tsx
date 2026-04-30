62ff06dd213c8860cddd6a158676a9aa1b9dffa6
Ahmed Alghali <a7medalghali777@gmail.com>
2026-05-01 01:11:38 +0300

    here we go

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/csdecoded.github.io">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/courses" element={<Courses />} />
