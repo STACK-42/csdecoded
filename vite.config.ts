114803b15f3d4d56c0667ae34f7b73acff1ae20e
Ahmed Alghali <a7medalghali777@gmail.com>
2026-05-01 01:08:20 +0300

    vite base changed

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import mdx from "@mdx-js/rollup";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/csdecoded.github.io/",
   server: {
     host: "::",
     port: 8080,
